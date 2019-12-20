d1e9bd36fde22194dc91a2915dddc7bc98a8e425
Max Pijittum <mpijittum@fb.com>
2019-12-20 15:11:57 +0800

    add interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,109 @@
+import { EventEmitter } from 'events';
+
+export enum ExperimentOperator {
+    IN = <any>'IN',
+    NOT_IN = <any>'NOT_IN',
+}
+
+export enum ExperimentParamType {
+    STRING = 'string',
+}
+
+export interface IExperimentContext {
+    [key: string]: string | undefined | number;
+    userId?: string;
+    sessionId?: string;
+    ipAddress?: string;
+    environment?: string;
+    appName?: string;
+    properties?: any;
+}
+
+export interface IExperiment {
+    name: string;
+    description?: string;
+    enabled: boolean;
+    strategies: IExperimentStrategyInfo[];
+    variants: IExperimentVariantDefinition[];
+}
+
+export interface IExperimentStrategy {
+    name: string;
+    enabled: boolean;
+    checkConstraint(constraint: IExperimentConstraint, context: IExperimentContext): boolean;
+    checkConstraints(constraints: IExperimentConstraint[], context: IExperimentContext): boolean;
+    isEnabled(params: any, context: IExperimentContext): boolean;
+    isEnableWithConstraint(params: any, context: IExperimentContext, constraints: IExperimentConstraint[]): boolean;
+}
+
+export interface IExperimentStrategyInfo {
+    name: string;
+    parameters: any;
+    constraints: IExperimentConstraint[];
+}
+
+export interface IExperimentConstraint {
+    contextName: string;
+    operator: ExperimentOperator;
+    values: string[];
+}
+
+export interface IExperimentParam {
+    name: string;
+    type: ExperimentParamType;
+    value: string;
+}
+
+export interface IExperimentVariantDefinition {
+    name: string;
+    weight: number;
+    params: IExperimentParam[];
+}
+
+export interface IExperimentVariant {
+    name: string;
+    enabled: boolean;
+    params: IExperimentParam[];
+}
+
+export interface IExperimentClient {
+    isEnable(name: string, context: IExperimentContext, fallback?: any):boolean;
+    isEnable(experiment: IExperiment, context: IExperimentContext, fallback: Function): boolean;
+    getVariant(name: string, context: IExperimentContext, fallback?: IExperimentVariant): IExperimentVariant;
+    getVariant(experiment: IExperiment, context: IExperimentContext, fallback?: IExperimentVariant): IExperimentVariant;
+}
+
+export interface IExperimentPlatform {
+    client: IExperimentClient;
+    destroy(): void;
+    getVariant(name: string, context: IExperimentContext, fallback?: IExperimentVariant): IExperimentVariant;
+    isEnabled(name: string, context: IExperimentContext, fallback?: Function): boolean;
+}
+
+export interface CustomHeaders {
+    [key: string]: string;
+}
+
+export type CustomHeadersFunction = () => Promise<CustomHeaders>;
+
+export interface IExperimentPlatformConfig {
+    appName: string;
+    environment?: string;
+    instanceId?: string;
+    url: string;
+    refreshInterval?: number;
+    metricsInterval?: number;
+    disableMetrics?: boolean;
+    backupPath?: string;
+    strategies?: IExperimentStrategyInfo[];
+    customHeaders?: CustomHeaders;
+    customHeadersFunction?: CustomHeadersFunction;
+    timeout?: number;
+    repository?: IExperimentRepository;
+}
+
+export interface IExperimentRepository extends EventEmitter {
+    getExperiment(name: string): IExperiment;
+    getExperiments(): IExperiment[];
+    stop(): void;
+}
