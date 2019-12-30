361ecddcd9676db6ea752bdc187ef703c175ebfb
Max Pijittum <mpijittum@fb.com>
2019-12-30 22:29:47 +0700

    add snapshot test for client

--- src/__test__/client/client.test.ts
+++ src/__test__/client/client.test.ts
@@ -105,7 +105,7 @@ test('should use custom strategy', () => {
         instanceId,
         refreshInterval: 0
     }, () => buildToggle('feature', true, [{ name: 'custom', parameters: {}, constraints: [] }]))
-    
+
 
     const client = new Client(repo, [new DefaultStrategy(), new CustomStrategy()]);
     client.on('error', log).on('warn', log);
@@ -121,7 +121,7 @@ test('should use a set of custom strategies', () => {
         appName,
         instanceId,
         refreshInterval: 0
-    }, () => buildToggle('feature', true, [{ name: 'custom', parameters: {}, constraints: []  }, { name: 'custom-false' , parameters: {}, constraints: [] }]))
+    }, () => buildToggle('feature', true, [{ name: 'custom', parameters: {}, constraints: [] }, { name: 'custom-false', parameters: {}, constraints: [] }]))
 
     const strategies = [new CustomFalseStrategy(), new CustomStrategy()];
     const client = new Client(repo, strategies);
@@ -152,17 +152,20 @@ test('should return false a set of custom-false strategies', () => {
 });
 
 test('should emit error when invalid feature runtime', () => {
+    expect.assertions(3)
     const repo = new MockRepository({
         backupPath: 'foo',
         url: 'http://experiment-server-client-01.mahasak.com',
         appName,
         instanceId,
         refreshInterval: 0
-    }, () => { return {
-        name: 'feature-malformed-strategies',
-        enabled: true,
-        strategies: true,
-    }})
+    }, () => {
+        return {
+            name: 'feature-malformed-strategies',
+            enabled: true,
+            strategies: true,
+        }
+    })
 
     const strategies: IExperimentStrategy[] = [new DefaultStrategy()];
     const client = new Client(repo, strategies);
@@ -170,7 +173,93 @@ test('should emit error when invalid feature runtime', () => {
         expect(err).toBeTruthy();
         expect(err.message).toContain('Malformed feature');
     })
-    
+
     expect(client.isEnabled('feature-malformed-strategies')).toBeFalsy();
-    expect
+});
+
+
+
+test('should emit error when mising feature runtime', () => {
+    expect.assertions(3);
+
+    const repo = new MockRepository({
+        backupPath: 'foo',
+        url: 'http://experiment-server-client-01.mahasak.com',
+        appName,
+        instanceId,
+        refreshInterval: 0
+    }, () => {
+        return {
+            name: 'feature-wrong-strategy',
+            enabled: true,
+            strategies: [new CustomFalseStrategy()],
+        }
+    })
+
+    const strategies: IExperimentStrategy[] = [new DefaultStrategy()];
+    const client = new Client(repo, strategies);
+    client.on('error', log);
+    client.on('warn', msg => {
+        expect(msg).toBeTruthy();
+        expect(msg).toContain('Missing strategy');
+    });
+
+    expect(client.isEnabled('feature-wrong-strategy')).toBeFalsy()
+});
+
+[
+    [['y', 1], ['0', 1], ['1', 1]],
+    [['3', 33], ['2', 33], ['0', 33]],
+    [['aaa', 100], ['3', 100], ['1', 100]],
+].forEach(([[id1, weight1], [id2, weight2], [id3, weight3]]) => {
+    test(`should return variant when equal weight on ${weight1},${weight2},${weight3}`, () => {
+        const repo = new MockRepository({
+            backupPath: 'foo',
+            url: 'http://experiment-server-client-01.mahasak.com',
+            appName,
+            instanceId,
+            refreshInterval: 0
+        }, () => {
+            return buildToggle('feature', true, [], [
+                {
+                    name: 'variant1',
+                    weight: parseInt(weight1.toString()),
+                    params: [{
+                        name: 'p1',
+                        type: 'string',
+                        value: 'val1',
+                    }],
+                },
+                {
+                    name: 'variant2',
+                    weight: parseInt(weight2.toString()),
+                    params: [{
+                        name: 'p1',
+                        type: 'string',
+                        value: 'val2',
+                    }],
+                },
+                {
+                    name: 'variant3',
+                    weight: parseInt(weight3.toString()),
+                    params: [{
+                        name: 'p1',
+                        type: 'string',
+                        value: 'val3',
+                    }],
+                },
+            ]);
+        });
+
+        const strategies = [new DefaultStrategy()];
+        const client = new Client(repo, strategies);
+        client.on('error', log).on('warn', log);
+        const result = client.isEnabled('feature');
+
+        expect(result).toBeTruthy();
+
+        [id1, id2, id3].forEach(id => {
+            expect(client.getVariant('feature', { userId: id.toString() })).toMatchSnapshot()
+        });
+    });
 });
